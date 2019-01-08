fd4b83ff1b51961245c3231d91f51a9c492a3105
Joey Rivera <joey7040@hotmail.com>
2019-01-08 15:15:23 -0500

    Added entry form for the recipe list.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
-import  RecipeList  from './RecipeList'
+import  RecipeList  from './RecipeList';
+import RecipeInput from './RecipeInput';
 import Navbar from './Navbar';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "pasta.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "milkshake.jpg"
+        },
+        { 
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "avocadoToast.jpg"
+        }
+      ],
+      nextRecipeId: 3,
+      showForm: false
+    }
+    
+    this.handleSave = this.handleSave.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+  }
+  
+  handleSave(recipe) {
+    this.setState((prevState, props) => {
+      const newRecipe = {...recipe, id: this.state.nextRecipeId};
+      return {
+        nextRecipeId: prevState.nextRecipeId + 1,
+        recipes: [...this.state.recipes, newRecipe],
+        showForm: false
+      }
+    });
+  }
+  
+  onDelete(id) {
+    const recipes = this.state.recipes.filter(r => r.id !== id);
+    this.setState({recipes});
+  }
+  
   render() {
+    const {showForm} = this.state;
     return (
       <div className="App">
-        <Navbar />
-        <RecipeList />
+        <Navbar onNewRecipe={() => this.setState({showForm: true})} />
+        { showForm ?
+            <RecipeInput 
+              onSave={this.handleSave}
+              onClose={() => this.setState({showForm: false})}  
+            /> :
+            null }
+        <RecipeList onDelete={this.onDelete} recipes={this.state.recipes} />
       </div>
     );
   }
 }
-
 export default App;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,36 +4,15 @@ import PropTypes from 'prop-types';
 import './RecipeList.css';
 
 class RecipeList extends Component {
-  static defaultProps = {
-    recipes: [
-      {
-        title: "Spaghetti",
-        instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
-        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-        img: "pasta.jpg"
-      },
-      {
-        title: "Milkshake",
-        instructions: "Combine ice cream and milk.  Blend until creamy",
-        ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-        img: "milkshake.jpg"
-      },
-      {
-        title: "Avocado Toast",
-        instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
-        ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
-        img: "avocadoToast.jpg"
-      }
-    ]
-  }
-  
   static propTypes = {
-    recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+    recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onDelete: PropTypes.func.isRequired
   }
   
   render() {
+    const {onDelete} = this.props;
     const recipes = this.props.recipes.map((r,index) => (
-      <Recipe key={index} {...r} />
+      <Recipe key={r.id} {...r} onDelete={onDelete} />
     ));
     
     return (
